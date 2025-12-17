077b28fad9dcd0f4e52191634150c5e94b5dd6e3
Carlos Eduardo Medim <33000080+kadumedim@users.noreply.github.com>
2025-12-17 21:09:00 +0000

    rename bind port

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -eu
 #   INFISICAL1_HOST, INFISICAL2_HOST, INFISICAL3_HOST
 # Optional:
 #   INFISICAL_PORT   (default: 8080)
-#   BIND_PORT        (default: 8080)   - HAProxy public HTTP port
+#   PORT        (default: 8080)   - HAProxy public HTTP port
 #   STATS_PORT       (default: 8404)
 #   STATS_PASSWORD   (optional; if empty, stats page has no auth)
 #   RESOLVE_PREFER   (default: ipv4)   - ipv4 | ipv6
@@ -15,16 +15,16 @@ set -eu
 : "${INFISICAL3_HOST:?INFISICAL3_HOST is required}"
 
 INFISICAL_PORT="${INFISICAL_PORT:-8080}"
-BIND_PORT="${BIND_PORT:-8080}"
+PORT="${PORT:-8080}"
 STATS_PORT="${STATS_PORT:-8404}"
 RESOLVE_PREFER="${RESOLVE_PREFER:-ipv4}"
 STATS_PASSWORD="${STATS_PASSWORD:-}"
 
 export INFISICAL1_HOST INFISICAL2_HOST INFISICAL3_HOST \
-  INFISICAL_PORT BIND_PORT STATS_PORT RESOLVE_PREFER STATS_PASSWORD
+  INFISICAL_PORT PORT STATS_PORT RESOLVE_PREFER STATS_PASSWORD
 
 # Render template
-envsubst '${INFISICAL1_HOST} ${INFISICAL2_HOST} ${INFISICAL3_HOST} ${INFISICAL_PORT} ${BIND_PORT} ${STATS_PORT} ${RESOLVE_PREFER} ${STATS_PASSWORD}' \
+envsubst '${INFISICAL1_HOST} ${INFISICAL2_HOST} ${INFISICAL3_HOST} ${INFISICAL_PORT} ${PORT} ${STATS_PORT} ${RESOLVE_PREFER} ${STATS_PASSWORD}' \
   < /usr/local/etc/haproxy/haproxy.template.cfg \
   > /usr/local/etc/haproxy/haproxy.cfg
 
